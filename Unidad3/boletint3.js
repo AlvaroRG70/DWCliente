1be91f8837aefb1238f33560facfcef2eceb965f
Alvaro Rodriguez <alvaro.rodriguez.garrido.al@iespoligonosur.org>
2023-10-06 14:27:06 +0200

    actualizado

--- Unidad3/boletint3.js
+++ Unidad3/boletint3.js
@@ -23,12 +23,32 @@ document.write(contadorC + ' nombres contienen la C y '+ contadorñ+ ' contienen
 */
 
 /*
-2. Leer un texto de la entrada estándar y contar cuántas palabras tiene, cuántas de ellas empiezan por A. Suponemos que una palabra está separada de otra por uno o más espacios, un tabulador y el texto acaba con un punto, no existe ningún punto y aparte y las palabras son todas correctas.
+a)  Comprobar que el nombre de usuario contiene al menos tres letras. Si no es así, debe
+aparecer un mensaje por pantalla indicando que el texto no cumple tener al menos tres
+letras. Debe aparecer el mensaje: “El nombre no cumple tener al menos tres letras”.
+EJERCICIO PPT
+*/
+/*
+var nombre_us = "Alvaro@gmailcom"
+var er = /\w{3,}@/
+var er2 = /\w@\w+\.\w+/
+var er3 = /\sat\s/
+console.log(er.test(nombre_us))
+console.log(er2.test(nombre_us))
+console.log(nombre_us.replace(er3, '@'))
 */
 
-var texto = promtp("escribe un texto")
 
-texto.split([])
+/*
+2. Leer un texto de la entrada estándar y contar cuántas palabras tiene, cuántas de ellas empiezan por A. Suponemos que una palabra está separada de otra por uno o más espacios, un tabulador y el texto acaba con un punto, no existe ningún punto y aparte y las palabras son todas correctas.
+*/
+var texto = "hola si adios no alvaro alberto luis"
+er = /(\w+\s|\w+$)/
+console.log(texto.match(er))
+
+
+
+
 
 
 
