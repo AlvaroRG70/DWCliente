406b275de091dabc2ec19f45c458615fd905150a
Alvaro Rodriguez <rgalvaro70@gmail.com>
2024-01-17 20:14:00 +0100

    examen terminado

--- 2trimestudio/examen22/ej2/ej2.js
+++ 2trimestudio/examen22/ej2/ej2.js
@@ -3,6 +3,39 @@ window.addEventListener("load", inicializar)
 
 function inicializar() {
     
-    
+    let formulario = document.getElementById('formulario')
 
+    let texto = document.getElementById('codigo')
+    let er = /^[A-Z]{3}\-\d{1,5}$/
+
+    let p = document.createElement('p')
+
+    function validarTxt(){
+        if (!er.test(texto.value)){
+            
+            p.style.display = 'block'
+            p.innerHTML = 'texto no cumple las condiciones'
+            texto.insertAdjacentElement('afterend', p)
+            return
+
+        } else {
+            p.style.display = 'none'
+            p.innerHTML = ''
+        }
+    }
+
+    function validarForm() {
+        validarTxt()
+        if (p.style.display == 'block'){
+            alert('Hay errores, no podemos enviar el formulario')
+            formulario.reset()
+        } else {
+            alert('Formulario enviado correctamente')
+            formulario.submit()
+        }
+    }
+
+    texto.addEventListener('blur',validarTxt)
+    let boton = document.getElementById('boton')
+    boton.addEventListener('click',validarForm)
 }
